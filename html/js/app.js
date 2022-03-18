7bffb7d2d2e41c434620f15b705ac3cfc4c4b98e
TheF4llen08 <bradleyscarpi123@gmail.com>
2022-03-18 18:51:11 -0500

    Changed Eye color from white to grey

--- html/js/app.js
+++ html/js/app.js
@@ -4,7 +4,7 @@ const Targeting = Vue.createApp({
             Show: false,
             ChangeTextIconColor: false, // This is if you want to change the color of the icon next to the option text with the text color
             StandardEyeIcon: 'https://cdn.discordapp.com/attachments/903021216464531507/903024370665000981/normaleye.png', // Instead of icon it's using a image source found in HTML 
-            CurrentIcon: 'https://cdn.discordapp.com/attachments/903021216464531507/903024370665000981/normaleye.png', // Instead of icon it's using a image source found in HTML
+            CurrentIcon: 'https://cdn.discordapp.com/attachments/919672563578318848/954527263544717413/normaleye.png', // Instead of icon it's using a image source found in HTML
             SuccessIcon: 'https://cdn.discordapp.com/attachments/903021216464531507/903024373626208336/activeeye.png', // Instead of icon it's using a image source found in HTML
             SuccessColor: "rgb(5, 241, 178)",
             StandardColor: "white",
